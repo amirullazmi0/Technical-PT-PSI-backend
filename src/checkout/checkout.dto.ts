7877a275e291ae3313c5734f01ac4105a1a98da0
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-14 00:55:55 +0700

    Add createCheckoutDto and createCheckoutResponseDto for checkout processing

---
+++ src/checkout/checkout.dto.ts
@@ -0,0 +1,20 @@
+import { IsEmpty, IsNotEmpty, IsNumber, IsOptional } from "class-validator";
+
+export class createCheckoutDto {
+  @IsNumber()
+  @IsNotEmpty()
+  price: number;
+
+  @IsNumber()
+  @IsOptional()
+  qty: number;
+
+  @IsNumber()
+  @IsOptional()
+  voucher: number;
+}
+
+export interface createCheckoutResponseDto {
+  totalPrice: number,
+  point: number
+}
