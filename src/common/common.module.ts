428de181a9a190e7fe7200a1a8b73a64b6297123
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-14 00:56:10 +0700

    Refactor CommonModule to include global ConfigModule and JwtModule imports

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    JwtModule.register({
+      global: true,
+      secret: 'mysecret-technicaltest-pt-psi',
+    }),
+  ],
+})
 export class CommonModule { }
