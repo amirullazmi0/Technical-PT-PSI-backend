98a9596b0afbd465c83d53fc4f054168d1be5851
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-14 00:54:39 +0700

    Update server port configuration to use environment variable from ConfigService

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+
+  const env = app.get(ConfigService);
+  const port = env.get('PORT') || 4444;
+  await app.listen(port);
+  console.log(`Server running on port ${port}`);
 }
 bootstrap();
