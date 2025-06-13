c3187111d2ce67fa624ba613db5ee9f21bac4162
Amirull Azmi <amirullazmi0@gmail.com>
2025-06-14 00:56:02 +0700

    Implement createCheckout method in CheckoutService for processing checkout logic

--- src/checkout/checkout.service.ts
+++ src/checkout/checkout.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { createCheckoutDto, createCheckoutResponseDto } from './checkout.dto';
+import { WebResponse } from 'src/dto/Response';
 
 @Injectable()
-export class CheckoutService {}
+export class CheckoutService {
+  async createCheckout(
+    body: createCheckoutDto
+  ): Promise<WebResponse<createCheckoutResponseDto>> {
+    let totalPrice = body.price * (!body.qty ? 1 : body.qty);
+    let point = 0
+    if (body.voucher) {
+      const discount = (totalPrice * body.voucher)
+      totalPrice = totalPrice - discount;
+      point = discount * 0.02
+    }
+    return {
+      success: true,
+      message: 'create checkout success',
+      data: {
+        totalPrice: totalPrice,
+        point: point
+      }
+    }
+  }
+}
